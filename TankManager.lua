2ab7f15c4ac4bf1e55f9cc87cb5800eb45e110cc
Dan Wager <selmersaxman@gmail.com>
2013-05-11 13:57:43 -0700

    Rename of textures

---
+++ TankManager.lua
@@ -0,0 +1,27 @@
+TankManager = {}
+
+
+local tanks =
+{
+    ["red"] =
+    {
+        image = "tank_red",
+        barrelImage = "tank_red_barrel",
+        barrelOffsetX = 0,
+        barrelOffsetY = -16,
+        scale = .125,
+        power = 10,
+        barrelAngle = 0,
+        barrelSpeed = 50,
+        maxHp = 100,
+        hp = 100,
+        btnShoot =
+
+    }
+
+
+
+
+}
+
+tanks
