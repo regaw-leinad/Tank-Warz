1aae0b5757c40ed3183a58be413334fcb7825602
Dan Wager <selmersaxman@gmail.com>
2013-05-16 01:02:34 -0700

    Moved getTankDrop to TankManager

--- TankManager.lua
+++ TankManager.lua
@@ -61,6 +61,49 @@ local function get(tank)
     end
 end
 
+-- Gets the location and angle of a new tank
+-- @param x The X coordinate of the tank
+-- @return The X and Y coordinate and angle (CW = positive) of the tank (int, int, int)
+local function getTankDrop(x)
+    local terrain = EntityManager.getAll("terrain")[1]
+    local v = terrain:getCoords()
+    local points = terrain:getPointCount()
+
+    -- raises the tank a few pixels above the terrain
+    local dropBuf = 12
+
+    -- prevents the tank from dropping too close to a vertex
+    local vertexBuf = 15
+
+    for i = 1, points, 2 do
+        local leftX = v[i]
+        local leftY = v[i+1]
+        local rightX = v[i+2]
+        local rightY = v[i+3]
+
+        if leftX <= x and x <= rightX then
+            -- repositions the tank drop if too close to either vertex
+            if (leftX + vertexBuf) >= (rightX - vertexBuf) then
+                -- between leftX and rightX happens to be too narrow
+                x = (leftX + rightX) / 2
+            elseif x < (leftX + vertexBuf) then
+                -- too close to the left vertex
+                x = leftX + vertexBuf
+            elseif (rightX - vertexBuf) < x then
+                -- too close to the right vertex
+                x = rightX - vertexBuf
+            end
+
+            local resultY = (x - leftX) * (rightY - leftY) / (rightX - leftX) + leftY
+            local angle = math.atan2(rightY - leftY, rightX - leftX)
+
+            return x - dropBuf * math.sin(angle),
+                resultY - dropBuf * math.cos(angle),
+                math.deg(angle)
+        end
+    end
+end
+
 -- Gets a table containing the initialization data for the specified tank
 -- @param tank The tank
 -- @return The table of init data for the specified tank (table)
